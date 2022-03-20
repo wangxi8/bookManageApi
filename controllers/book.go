0ca93f8e9ba53593042bb58a3d313cb85a121409
wx <>
2022-03-20 12:47:52 +0800

    根据id获取图书详情

---
+++ controllers/book.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"bookManageApi/models"
+	"encoding/json"
+
+	"github.com/astaxie/beego"
+)
+
+// Operations about Users
+type UserController struct {
+	beego.Controller
+}
+
+// @Title CreateUser
+// @Description create users
+// @Param	body		body 	models.User	true		"body for user content"
+// @Success 200 {int} models.User.Id
+// @Failure 403 body is empty
+// @router / [post]
+func (u *UserController) Post() {
+	var user models.User
+	json.Unmarshal(u.Ctx.Input.RequestBody, &user)
+	uid := models.AddUser(user)
+	u.Data["json"] = map[string]string{"uid": uid}
+	u.ServeJSON()
+}
+
+// @Title GetAll
+// @Description get all Users
+// @Success 200 {object} models.User
+// @router / [get]
+func (u *UserController) GetAll() {
+	users := models.GetAllUsers()
+	u.Data["json"] = users
+	u.ServeJSON()
+}
+
+// @Title Get
+// @Description get book by id
+// @Param	id		path 	int	true		"The key for staticblock"
+// @Success 200 {object} models.book
+// @Failure 403 :id is empty
+// @router /:id [get]
+func (u *BookController) Get() {
+	id := u.GetString(":id")
+	if id != "" {
+		book, err := models.query(id)
+		if err != nil {
+			u.Data["json"] = err.Error()
+		} else {
+			u.Data["json"] = book
+		}
+	}
+	u.ServeJSON()
+}
+
+// @Title Delete
+// @Description delete the user
+// @Param	uid		path 	string	true		"The uid you want to delete"
+// @Success 200 {string} delete success!
+// @Failure 403 uid is empty
+// @router /:uid [delete]
+func (u *UserController) Delete() {
+	uid := u.GetString(":uid")
+	models.DeleteUser(uid)
+	u.Data["json"] = "delete success!"
+	u.ServeJSON()
+}
+
---
+++ models/book.go
@@ -0,0 +1,97 @@
+package models
+
+import (
+    "crud/routers"
+    "database/sql"
+    "fmt"
+
+    "github.com/go-sql-driver/mysql"
+)
+
+//增加数据
+func insert() {
+    db, err := sql.Open("mysql", "wx:wang...123@tcp(20.27.155.16:3306)/bookManage?charset=utf8")
+    checkErr(err)
+
+    stmt, err := db.Prepare(`INSERT user (userid,username,userage,usersex) values (?,?,?,?)`)
+    checkErr(err)
+    res, err := stmt.Exec(1, "Mary", 20, 1)
+    checkErr(err)
+    id, err := res.LastInsertId()
+    checkErr(err)
+    fmt.Println(id)
+}
+
+//删除数据
+func remove() {
+    db, err := sql.Open("mysql", "wx:wang...123@tcp(20.27.155.16:3306)/bookManage?charset=utf8")
+    checkErr(err)
+
+    stmt, err := db.Prepare(`DELETE FROM user WHERE userid=?`)
+    checkErr(err)
+    res, err := stmt.Exec(1)
+    checkErr(err)
+    num, err := res.RowsAffected()
+    checkErr(err)
+    fmt.Println(num)
+}
+
+//更新数据
+func update() {
+    db, err := sql.Open("mysql", "wx:wang...123@tcp(20.27.155.16:3306)/bookManage?charset=utf8")
+    checkErr(err)
+
+    stmt, err := db.Prepare(`UPDATE user SET userage=?,usersex=? WHERE userid=?`)
+    checkErr(err)
+    res, err := stmt.Exec(21, 2, 2)
+    checkErr(err)
+    num, err := res.RowsAffected()
+    checkErr(err)
+    fmt.Println(num)
+}
+
+//查询数据
+func queryAll() {
+    db, err := sql.Open("mysql", "wx:wang...123@tcp(20.27.155.16:3306)/bookManage?charset=utf8")
+    checkErr(err)
+
+    rows, err := db.Query("SELECT * FROM book")
+    checkErr(err)
+
+    //    //普通demo
+    for rows.Next() {
+        var name string
+        var isbn string
+        var author string
+        var number int
+        var location string
+        var mobile int
+        var wechat string
+        var desc string
+
+        rows.Columns()
+        err = rows.Scan(&userid, &username, &userage, &usersex)
+        checkErr(err)
+
+        fmt.Println(userid)
+        fmt.Println(username)
+        fmt.Println(userage)
+        fmt.Println(usersex)
+    }
+}
+
+func query(id string){
+	db, err := sql.Open("mysql", "wx:wang...123@tcp(20.27.155.16:3306)/bookManage?charset=utf8")
+    checkErr(err)
+
+    rows, err := db.Query("SELECT * FROM book where id = " + id)
+    checkErr(err)
+    return rows
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
+
+}
