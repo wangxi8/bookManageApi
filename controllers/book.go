28ab9eadccc1393439ed26adbfc4d642be9bf752
wx <>
2022-03-22 16:54:39 +0800

    debug

--- controllers/book.go
+++ controllers/book.go
@@ -19,7 +19,7 @@ type BookController struct {
 // @Failure 403 :id is empty
 // @router /:id [get]
 func (u *BookController) Get() {
-	id := u.GetString(":id")
+	id, _ := u.GetInt(":id")
 	if id != "" {
 		book, err := models.Query(id)
 		if err != nil {
@@ -40,10 +40,10 @@ func (u *BookController) Get() {
 // @Success 200 {object} models.Book
 // @router /getList [get]
 func (u *BookController) GetList() {
-	name     := u.GetString(":name")
-	isbn     := u.GetString(":isbn")
-	location := u.GetString(":location")
-	page     := u.GetInt(":page")
+	name      := u.GetString(":name")
+	isbn      := u.GetString(":isbn")
+	location  := u.GetString(":location")
+	page, _ := u.GetInt(":page")
 
 	var book models.Book
 
@@ -51,39 +51,36 @@ func (u *BookController) GetList() {
 	book.Isbn     = isbn
 	book.Location = location
 
-	if id != "" {
-		bookArr, err := models.QueryBookList(book, page)
-		if err != nil {
-			u.Data["json"] = err.Error()
-		} else {
-			u.Data["json"] = bookArr
-		}
+	bookArr, num, err := models.QueryBookList(&book, page)
+	if err != nil {
+		u.Data["json"] = err.Error()
+	} else {
+		u.Data["json"] = bookArr
 	}
 	u.ServeJSON()
 }
 
 // @Title in
 // @Description 插入一条图书
-// @Param	id		    query 	int	    true		"id"
 // @Param	name		query 	string	true		"书名"
 // @Param	isbn		query 	string	true		"isbn"
 // @Param	location    query 	string	true		"位置"
-// @Param	author		query 	int	    true		"作者"
-// @Param	number		query 	int	    false		"编号"
+// @Param	author		query 	string	true		"作者"
+// @Param	number		query 	string  false		"编号"
 // @Param	mobile		query 	int	    false		"手机号"
-// @Param	wechat		query 	int	    true		"微信号"
-// @Param	desc		query 	int	    false		"描述"
+// @Param	wechat		query 	string	true		"微信号"
+// @Param	desc		query 	string	false		"描述"
 // @Success 200 {object} models.Book
 // @router /insert [post]
 func (u *BookController) Insert() {
-	name     := u.GetString(":name")
-	isbn     := u.GetString(":isbn")
-	location := u.GetString(":location")
-	author   := u.GetInt(":author")
-	number   := u.GetInt(":number")
-	mobile   := u.GetInt(":mobile")
-	wechat   := u.GetInt(":wechat")
-	desc     := u.GetInt(":desc")
+	name      := u.GetString(":name")
+	isbn      := u.GetString(":isbn")
+	location  := u.GetString(":location")
+	author    := u.GetString(":author")
+	number    := u.GetString(":number")
+	mobile, _ := u.GetInt(":mobile")
+	wechat    := u.GetString(":wechat")
+	desc      := u.GetString(":desc")
 
 	var book models.Book
 
@@ -131,24 +128,24 @@ func (u *BookController) Remove() {
 // @Param	name		query 	string	true		"书名"
 // @Param	isbn		query 	string	true		"isbn"
 // @Param	location    query 	string	true		"位置"
-// @Param	author		query 	int	    true		"作者"
-// @Param	number		query 	int	    false		"编号"
+// @Param	author		query 	string	true		"作者"
+// @Param	number		query 	string  false		"编号"
 // @Param	mobile		query 	int	    false		"手机号"
-// @Param	wechat		query 	int	    true		"微信号"
-// @Param	desc		query 	int	    false		"描述"
+// @Param	wechat		query 	string	true		"微信号"
+// @Param	desc		query 	string	false		"描述"
 // @Success 200 {object} models.Book
 // @Failure 403 :id is empty
 // @router /update [post]
 func (u *BookController) Update() {
-	id       := u.GetString(":id")
-	name     := u.GetString(":name")
-	isbn     := u.GetString(":isbn")
-	location := u.GetString(":location")
-	author   := u.GetInt(":author")
-	number   := u.GetInt(":number")
-	mobile   := u.GetInt(":mobile")
-	wechat   := u.GetInt(":wechat")
-	desc     := u.GetInt(":desc")
+	id        := u.GetString(":id")
+	name      := u.GetString(":name")
+	isbn      := u.GetString(":isbn")
+	location  := u.GetString(":location")
+	author    := u.GetString(":author")
+	number    := u.GetString(":number")
+	mobile, _ := u.GetInt(":mobile")
+	wechat    := u.GetString(":wechat")
+	desc      := u.GetString(":desc")
 
 	var book models.Book
 
